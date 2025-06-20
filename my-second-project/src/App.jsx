1863d8bfcb122a83c86017c3d0d4210c3e9b86a5
Dejosh055 <dejosh055@gmail.com>
2025-06-20 14:45:20 +0100

    second task done

---
+++ my-second-project/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className="card">
+        <h2>Count:{count}</h2>
+        <button onClick={() => setCount((count) => count + 1)}>
+          <h3>INCREAMENT</h3>
+        </button>
+
+        <button onClick={() => setCount((count) => count - 1)}>
+         <h3>DECREASE</h3>
+        </button>
+       
+      </div>
+    </>
+  )
+}
+
+export default App
